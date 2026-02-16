ec8796b3a0332137930ed8a2dc31a6a20643c2d8
bobblejotcake <seanlimhe@gmail.com>
2026-02-16 13:37:09 +1000

    Fix a bug that did not detect division as part of the REGEX expression

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function divide(num1,num2){
 //Output: an integer
 function operate(func, num1, num2){
     let ans = "no answer";
-    console.log(func);
     switch(func){
         case "+":
             ans = add(num1,num2);
@@ -59,7 +58,7 @@ let readyToCalc = false;
 let readyToCalcStorage = false;
 let allowNewOperator = false;
 const regex = /[+\-*/=]/;
-const regexSymbols = /[+\-*]/;
+const regexSymbols = /[+\-*/]/;
 
 //TO DO
 
@@ -70,9 +69,12 @@ const regexSymbols = /[+\-*]/;
 
 let buttons = document.querySelector(".container");
 buttons.addEventListener("click", (e) => {
-    // console.log(e.target);
     console.log(allowNewOperator);
     if (e.target.classList.contains("add")){
+        // Check if there is more than one operator in either storage or current
+        //If so, pop the last indicator out if current
+        //Or prevent if stored + error message.
+        
         if(allowNewOperator && regexSymbols.test(e.target.textContent)){
             storedArray.push(e.target.textContent);
         }
@@ -80,10 +82,7 @@ buttons.addEventListener("click", (e) => {
             currentArray.push(e.target.textContent);
         }
         allowNewOperator = false;
-        // Check if there is more than one operator in either storage or current
-        //If so, pop the last indicator out if current
-        //Or prevent if stored + error message.
-        
+     
         displayNumbers(currentArray);
     }
     else if (e.target.classList.contains("clear")){
@@ -95,12 +94,34 @@ buttons.addEventListener("click", (e) => {
         currentArray.pop();
         displayNumbers(currentArray);
     }
-
-
     
     let testing = ArrayCalculate(currentArray);
     let mergeCurrentAndStored = storedArray.concat(currentArray);
     let testingStorage = ArrayCalculate(mergeCurrentAndStored);
+
+    //If more than one operator is placed - ignore the first one
+    //Check if storage is filled and is an operator
+    console.log(`Checking time! ${storedArray}`);
+    if(storedArray && regexSymbols.test(storedArray[storedArray.length-1])){
+        if(regexSymbols.test(currentArray[currentArray.length-1])){
+            alert("You have too many conscutive operations!");
+            currentArray.pop();
+            displayNumbers(currentArray);
+        }
+    } else{
+        let testOperation1 = currentArray[currentArray.length-1];
+        let testOperation2 = currentArray[currentArray.length-2];
+        if (regexSymbols.test(testOperation1) && regexSymbols.test(
+            testOperation2
+        )){
+            alert("You have too many conscutive operations!");
+            currentArray.pop();
+            displayNumbers(currentArray);
+        }
+    }
+
+
+    //If not check the current array for two
 
     if(testing[0] != undefined && Number.isInteger(testing[1]) 
         && Number.isInteger(testing[2])){
@@ -121,12 +142,12 @@ buttons.addEventListener("click", (e) => {
 
     
     
-    if(readyToCalc == true){
+    if(readyToCalc){
         if(regex.test(e.target.textContent)){
             let validArray = ArrayCalculate(currentArray);
             let finalAns = operate(validArray[0],validArray[1],validArray[2]);
             currentArray = [];
-            storedArray = [finalAns]
+            storedArray = [Math.floor(finalAns)]
             displayNumbers(storedArray);
             if (regexSymbols.test(e.target.textContent)){
                 storedArray.push(e.target.textContent);
@@ -141,7 +162,7 @@ buttons.addEventListener("click", (e) => {
             let validArray = ArrayCalculate(mergeCurrentAndStored);
             let finalAns = operate(validArray[0],validArray[1],validArray[2]);
             currentArray = [];
-            storedArray = [finalAns]
+            storedArray = [Math.floor(finalAns)];
             displayNumbers(storedArray);
             if (regexSymbols.test(e.target.textContent)){
                 storedArray.push(e.target.textContent);
@@ -163,4 +184,8 @@ function ArrayCalculate(currentArray){
         let num1 = parseInt(currentArray.slice(0, test).join(""));
         let num2 = parseInt(currentArray.slice(test+1).join(""));
         return [currentArray[test],num1,num2];
+}
+
+function CheckOperators(currentArray){
+
 }
